a824b542ba11e561430d7f4664cf3e1471621325
L <1009621693@qq.com>
2024-08-15 01:13:37 +0800

    first

---
+++ shared_bike/BusProcessor.h
@@ -0,0 +1,24 @@
+﻿#ifndef __BUSPROCESSOR_H_
+#define __BUSPROCESSOR_H_
+
+#include <memory>
+#include "user_event_handler.h"
+#include "sqlconnection.h"
+#include "SqlTables.h"
+
+class BusinessProcessor
+{
+public:
+	BusinessProcessor(std::shared_ptr<MysqlConnection> sqlconn);
+	virtual ~BusinessProcessor();
+
+	bool init();
+
+private:
+	std::shared_ptr<MysqlConnection> mysqlconn_;
+	//MysqlConnection* mysqlcon;
+	std::shared_ptr<UserEventHandler> ueh_;
+};
+
+
+#endif // !__BUSPROCESSOR_H_
